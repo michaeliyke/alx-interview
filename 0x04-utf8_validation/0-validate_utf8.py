989f0a3e0af378f36af0d988cd342e2624358867
Michael Iyke <ymichaelc@gmail.com>
2024-07-24 12:39:27 +0100

    Project: UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,7 @@
 
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding"""
-    num_bytes = 0
+    """num_bytes = 0
 
     for num in data:
         mask = 1 << 7
@@ -24,3 +24,19 @@ def validUTF8(data):
         num_bytes -= 1
 
     return num_bytes == 0
+    """
+    """bytes = 0
+    for byte in data:
+        if bytes:
+            if byte >> 6 != 2:
+                return False
+            bytes -= 1
+            continue
+        while (1 << abs(7 - bytes)) & byte:
+            bytes += 1
+        if bytes == 1 or bytes > 4:
+            return False
+        bytes = max(bytes - 1, 0)
+    return bytes == 0
+    """
+    pass
