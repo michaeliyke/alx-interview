83d7f7ae260e1e40ce46d830f0a8cf88817ffc8e
Michael Iyke <ymichaelc@gmail.com>
2024-08-07 22:27:55 +0100

    correct

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@
 const request = require('request');
 const movieId = process.argv[2];
 
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
   if (error) {
@@ -15,17 +15,25 @@ request(url, (error, response, body) => {
     const film = JSON.parse(body);
     const characters = film.characters;
 
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-        } else if (response.statusCode !== 200) {
-          console.error('Status:', response.statusCode);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
+    // Use Promise.all to process all characters with order preserved
+    Promise.all(characters.map(processCharacter))
+      .then((names) => names.forEach((name) => console.log(name)))
+      .catch((error) => console.error('Error:', error));
   }
+
 });
+
+function processCharacter(characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(response.statusCode);
+      } else {
+        const character = JSON.parse(body);
+        resolve(character.name);
+      }
+    });
+  });
+}
