feb7fb2b906724e3e446a0f69099c071f1281b91
Michael Iyke <ymichaelc@gmail.com>
2024-08-07 22:03:53 +0100

    Task 0: 0. Star Wars Characters

---
+++ 0x06-starwars_api/main.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+// learning about the requests in node
+
+const https = require('https');
+const url = 'https://reqres.in/api/users';
+
+https.get(url, (res) => {
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    const users = JSON.parse(data);
+    console.log(users);
+  });
+
+}
+).on('error', (err) => {
+  console.log('Error: ' + err.message);
+}
+);
+
+
