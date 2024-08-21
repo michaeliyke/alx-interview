a059a9a78876de45fc17c6304d8c2ca6d39c7a56
Michael Iyke <ymichaelc@gmail.com>
2024-08-21 13:03:41 +0100

    correct

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,7 @@
 
 
 def makeChange(coins, total):
-    """Coin change making algorithm"""
+    """Calculate the minimum number of coins needed to make a given total"""
     if total <= 0:
         return 0
 
@@ -11,14 +11,15 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    # Iterate through each coin value to update the minimum number of
-    # coins needed for each total value
+    # Iterate through each coin
     for coin in coins:
-        # Updating takes place here
+        # Update the dp array for each value from coin to total
         for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+            if dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If infinite still, then the total can't be met
+    # If dp[total] is still infinity
+    # it means it's not possible to make that amount with the given coins
     if dp[total] == float('inf'):
         return -1
 
